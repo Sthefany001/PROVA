f0f87194f2916377ada4c06bd1318f8ff93097f4
Sthefany <s532401ed@gmail.com>
2023-06-13 15:29:02 -0400

    cinco commit

--- PROVA 2bim/Gabarito.cs
+++ PROVA 2bim/Gabarito.cs
@@ -25,11 +25,16 @@ namespace PROVA_2bim
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Metodo();
+        }
+
+         private void Metodo()
+         {
             int acertos = 0;
-            string[] gab = {"A", "B", "D", "E", "C", "A", "A", "D", "E", "B"};                    
+            string[] gab = { "A", "B", "D", "E", "C", "A", "A", "D", "E", "B" };
 
             try
-            {             
+            {
 
                 if (textBox1.Text.ToUpper() == gab[0])
                 {
@@ -38,64 +43,62 @@ namespace PROVA_2bim
 
                 if (textBox2.Text.ToUpper() == gab[1])
                 {
-                    acertos = acertos + 1;           
+                    acertos = acertos + 1;
                 }
-                
+
                 if (textBox3.Text.ToUpper() == gab[2])
                 {
                     acertos = acertos + 1;
                 }
-                
+
                 if (textBox4.Text.ToUpper() == gab[3])
                 {
                     acertos = acertos + 1;
                 }
-                
+
                 if (textBox5.Text.ToUpper() == gab[4])
                 {
                     acertos = acertos + 1;
                 }
-                
+
                 if (textBox6.Text.ToUpper() == gab[5])
                 {
                     acertos = acertos + 1;
                 }
-                
+
                 if (textBox7.Text.ToUpper() == gab[6])
                 {
                     acertos = acertos + 1;
                 }
-                
+
                 if (textBox8.Text.ToUpper() == gab[7])
                 {
                     acertos = acertos + 1;
                 }
-                
+
                 if (textBox9.Text.ToUpper() == gab[8])
                 {
                     acertos = acertos + 1;
                 }
-                
+
                 if (textBox10.Text.ToUpper() == gab[9])
                 {
                     acertos = acertos + 1;
                 }
-                
-                
+
+
                 //MessageBox.Show($"ACERTOS = {acertos.ToString()}");
 
                 label1.Text = acertos.ToString();
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"ERRO!{ex.Message}");
+                MessageBox.Show(ex.Message);
             }
             finally
             {
                 //MessageBox.Show("Obrigada por usar meu programa!");
             }
-
-           
         }
     }
 }
